06e6f046738a8f346ac548aaa6ca8d2ff02cc058
Octave Lapchin <46896739+OctaveLapchin@users.noreply.github.com>
2025-06-21 17:31:27 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -8,7 +8,7 @@ function checkApacheStatus(){
     },
     success: function(response) {
         console.log(response);
-        window.location.href = "https://noctave.fr";
+        window.location.href = "https://noctave.fr/portfolio";
     },
     error: function(xhr, status, error) {
         console.error("Erreur lors de la requête:", error);
